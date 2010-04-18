a14ce162de8673535eda108a62576d5d100394e1
Warnegia <Warnegia@2e6a1338-3172-bb72-0ab9-8434c77d5b8a>
2010-04-18 19:07:13 +0000

    milestone

--- action.ml
+++ action.ml
@@ -2,9 +2,8 @@ open Misc
   
 open XHTML.M
 
-let display connection domain page right = 
+let display source = 
   try  
-    let source = Db.load connection domain page right in
     let lexbuf = Lexing.from_string source in
       Wikiparser.type_block Wikilexer.token lexbuf
   with _ -> [div ~a:[a_class ["error"]] [pcdata "SYNTAX ERROR"]]  
